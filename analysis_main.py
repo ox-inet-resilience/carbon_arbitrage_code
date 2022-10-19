5d9e9537dfe62441264bfb234021a65a43a0bea7
climatefinance <105055284+climatefinance@users.noreply.github.com>
2022-10-19 08:02:15 -0400

    feat: Implement battery long term

--- analysis_main.py
+++ analysis_main.py
@@ -59,6 +59,7 @@ WEIGHT_GAS = None
 # This is used in Bruegel analysis. Might be deleted later.
 INVESTMENT_COST_DIVIDER = 1
 ENABLE_BATTERY_SHORT = True
+ENABLE_BATTERY_LONG = True
 
 print("Use new method:", ENABLE_NEW_METHOD)
 print("Renewable degradation:", ENABLE_RENEWABLE_GRADUAL_DEGRADATION)
@@ -70,6 +71,7 @@ print("Sector included", SECTOR_INCLUDED)
 print("BENEFIT NET GROWTH", ENABLE_BENEFIT_NET_GROWTH)
 print("WEIGHT_GAS", WEIGHT_GAS)
 print("BATTERY_SHORT", ENABLE_BATTERY_SHORT)
+print("BATTERY_LONG", ENABLE_BATTERY_LONG)
 
 if ENABLE_NEW_METHOD:
     assert ENABLE_RENEWABLE_GRADUAL_DEGRADATION or ENABLE_RENEWABLE_30Y_LIFESPAN
@@ -469,8 +471,15 @@ class InvestmentCostNewMethod:
         self.alpha_battery_short = self.alpha_2020_short_per_GJ / (
             self.G_battery_short_2020**-self.gamma_battery_short
         )
+        self.G_battery_long_2020 = 80.24 * 1e3  # kW
+        self.gamma_battery_long = -math.log2(1 - 0.086)
+        # The 1313 is in $/kW
+        self.alpha_2020_long_per_kW = 1313
+        self.alpha_battery_long = self.alpha_2020_long_per_kW / (self.G_battery_long_2020**-self.gamma_battery_long)
+        self.sigma_battery_long = 1 / 12
         self.stocks_kW = {tech: {} for tech in self.techs}
         self.stocks_GJ_battery_short = defaultdict(dict)
+        self.stocks_kW_battery_long = defaultdict(dict)
 
         # To be used in the full cost1 table calculation
         self.cost_non_discounted = []
@@ -568,6 +577,33 @@ class InvestmentCostNewMethod:
         self.stocks_GJ_battery_short[year][country_name] = D_battery_short
         return investment_cost_battery_short
 
+    def calculate_ic_1country_battery_long(self, year, country_name, total_R):
+        psi = 0.7
+        fe = 0.5
+        # kW
+        S_battery_long = self.get_stock_battery_long(year, country_name)
+        # kW
+        G_long = max(0, self.GJ2kW(total_R) * self.sigma_battery_long / psi / fe - S_battery_long)
+
+        # Calculating unit_ic
+        stock_without_degradation = 0.0
+        for stock_year, stock_amount in self.stocks_kW_battery_long.items():
+            if stock_year >= year:
+                break
+            stock_without_degradation += sum(stock_amount.values())
+        # kW
+        cumulative_G = self.G_battery_long_2020 + stock_without_degradation
+        # $/kW
+        if ENABLE_WRIGHTS_LAW:
+            unit_ic = self.alpha_battery_long * (cumulative_G**-self.gamma_battery_long)
+        else:
+            unit_ic = self.alpha_2020_long_per_kW
+        # End of calculating unit_ic
+
+        investment_cost_battery_long = G_long * unit_ic
+        self.stocks_kW_battery_long[year][country_name] = G_long
+        return investment_cost_battery_long
+
     def calculate_investment_cost_one_country(
         self, country_name, DeltaP, year, discount
     ):
@@ -584,6 +620,7 @@ class InvestmentCostNewMethod:
                 else:
                     self.stocks_kW[tech][year] = {country_name: 0.0}
             self.stocks_GJ_battery_short[year][country_name] = 0.0
+            self.stocks_kW_battery_long[year][country_name] = 0.0
             return
         # in kW because installed_costs is in $/kW
         D_kW = self.GJ2kW(D)
@@ -608,6 +645,13 @@ class InvestmentCostNewMethod:
             investment_cost += investment_cost_battery_short
         else:
             investment_cost_battery_short = 0
+        if ENABLE_BATTERY_LONG:
+            investment_cost_battery_long = self.calculate_ic_1country_battery_long(
+                year, country_name, total_R
+            )
+            investment_cost += investment_cost_battery_long
+        else:
+            investment_cost_battery_long = 0
         self.cost_non_discounted[-1][country_name] = investment_cost
         self.cost_discounted[-1][country_name] = investment_cost * discount
 
@@ -653,6 +697,22 @@ class InvestmentCostNewMethod:
             s = stock_amount[country_name]
             if ENABLE_RENEWABLE_30Y_LIFESPAN:
                 if age <= 12:
+                    out += s
+            else:
+                out += s
+        return out
+
+    def get_stock_battery_long(self, year, country_name):
+        out = 0.0
+        if len(self.stocks_kW_battery_long) == 0:
+            return out
+        for stock_year, stock_amount in self.stocks_kW_battery_long.items():
+            if stock_year >= year:
+                break
+            age = year - stock_year
+            s = stock_amount[country_name]
+            if ENABLE_RENEWABLE_30Y_LIFESPAN:
+                if age <= 16:
                     out += s
             else:
                 out += s
