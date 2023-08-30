67f75898b0d812a9864f0ec8b976217b1b2e1832
climatefinance <105055284+climatefinance@users.noreply.github.com>
2023-08-30 11:06:02 -0400

    scraper: Add non-masterdata countries

--- coal_export/worldbank_scraper.py
+++ coal_export/worldbank_scraper.py
@@ -4,8 +4,16 @@ import time
 
 import pandas as pd
 
+from coal_export.common_data import (
+    masterdata_alpha2,
+    masterdata_alpha3,
+    non_masterdata_alpha2,
+    non_masterdata_alpha3,
+)
+
+
 # Solved by https://stackoverflow.com/questions/71603314/ssl-error-unsafe-legacy-renegotiation-disabled
-#pd.read_excel("./import_2019_270111.xlsx")
+# pd.read_excel("./import_2019_270111.xlsx")
 products = {
     270111: "Coal; anthracite, whether or not pulverised, but not agglomerated",
     270112: "Coal; bituminous, whether or not pulverised, but not agglomerated",
@@ -13,24 +21,33 @@ products = {
     270120: "Briquettes, ovoids and similar solid fuels; manufactured from coal",
 }
 
-# Excluding XK
-masterdata_alpha2 = "AR AU BA BD BG BR BW CA CD CL CN CO CZ DE ES ET GB GE GR HU ID IN IR JP KG KZ LA ME MG MK MM MN MW MX MZ NE NG NO NZ PE PH PK PL RO RS RU SI SK TH TJ TR TZ UA US UZ VE VN ZA ZM ZW".split()
-# Excluding XK
-masterdata_alpha3 = "ARG AUS BIH BGD BGR BRA BWA CAN COD CHL CHN COL CZE DEU ESP ETH GBR GEO GRC HUN IDN IND IRN JPN KGZ KAZ LAO MNE MDG MKD MMR MNG MWI MEX MOZ NER NGA NOR NZL PER PHL PAK POL ROU SRB RUS SVN SVK THA TJK TUR TZA UKR USA UZB VEN VNM ZAF ZMB ZWE".split()
 masterdata_alpha3.append("WLD")
-for i, country in enumerate(masterdata_alpha3):
+
+alpha3s = non_masterdata_alpha3
+alpha2s = non_masterdata_alpha2
+if 0:
+    alpha3s = masterdata_alpha3
+    alpha2s = masterdata_alpha2
+
+for i, country in enumerate(alpha3s):
     for trade_flow in ["I", "E"]:
         for k, v in products.items():
-            a2 = masterdata_alpha2[i] if country != "WLD" else "WLD"
-            out_filename = f"./download_cache/{trade_flow}_{a2}_{k}.csv"
+            a2 = alpha2s[i] if country != "WLD" else "WLD"
+            out_filename = f"./coal_export/download_cache/{trade_flow}_{a2}_{k}.csv"
             if os.path.isfile(out_filename):
                 print("Skipping", out_filename)
                 continue
 
-            cmd = f"/run/current-system/sw/bin/wget https://wits.worldbank.org/Download.aspx?Reporter=ALL&Year=2019&Tradeflow={trade_flow}&Partner={country}&product={k}&Type=HS6Productdata&Lang=en -O download_cache/current.xlsx"
-            subprocess.check_output(cmd.split(" "), env={"OPENSSL_CONF": "openssl.conf"})
-            time.sleep(5)
-            df = pd.read_excel("./download_cache/current.xlsx", sheet_name="By-HS6Product")
+            cache_fname = f"current_{trade_flow}_{country}_{k}.xlsx"
+            cmd = f"/run/current-system/sw/bin/wget https://wits.worldbank.org/Download.aspx?Reporter=ALL&Year=2019&Tradeflow={trade_flow}&Partner={country}&product={k}&Type=HS6Productdata&Lang=en -O ./coal_export/download_cache/{cache_fname}"
+            subprocess.check_output(
+                cmd.split(" "), env={"OPENSSL_CONF": "./coal_export/openssl.conf"}
+            )
+            time.sleep(2.5)
+            df = pd.read_excel(
+                f"./coal_export/download_cache/{cache_fname}",
+                sheet_name="By-HS6Product",
+            )
             if len(df) == 0:
                 print("Empty", out_filename)
                 subprocess.check_output(["touch", out_filename])
