3ead1046efbe2e5b1c1795343a817914c3ccdc72
climatefinance <105055284+climatefinance@users.noreply.github.com>
2024-02-26 17:09:11 -0500

    Refactor coal_export/common.py

--- coal_export/common.py
+++ coal_export/common.py
@@ -8,6 +8,8 @@ production_2019 = nonpower_coal.groupby("asset_country")._2019.sum()
 masterdata_countries = set(
     "AR AU BA BD BG BR BW CA CD CL CN CO CZ DE ES ET GB GE GR HU ID IN IR JP KG KZ LA ME MG MK MM MN MW MX MZ NE NG NO NZ PE PH PK PL RO RS RU SI SK TH TJ TR TZ UA US UZ VE VN ZA ZM ZW".split()
 )
+cached_export_fraction = {}
+cached_import_fraction = {}
 
 
 def get_export_fraction(country):
@@ -63,59 +65,58 @@ def get_import_fraction(e, i):
     return fraction
 
 
-def modify_array_based_on_coal_export(arr, production_2019):
+def cached_get_export_fraction(country):
+    if country in cached_export_fraction:
+        fraction = cached_export_fraction[country]
+    else:
+        fraction = get_export_fraction(country)
+        cached_export_fraction[country] = fraction
+    return fraction
+
+
+def cached_get_import_fraction(e, i):
+    key = f"{e}_{i}"
+    if key in cached_import_fraction:
+        fraction = cached_import_fraction[key]
+    else:
+        fraction = get_import_fraction(e, i)
+        cached_import_fraction[key] = fraction
+    return fraction
+
+
+def modify_based_on_coal_export(element):
     from coal_export.common_data import non_masterdata_alpha2
-    from coal_export.common import (
-        get_export_fraction as common_get_export_fraction,
-        get_import_fraction as common_get_import_fraction,
-    )
 
-    cached_export_fraction = {}
-    cached_import_fraction = {}
-
-    def get_export_fraction(country):
-        if country in cached_export_fraction:
-            fraction = cached_export_fraction[country]
+    if isinstance(element, float):
+        # Do nothing if it is a scalar
+        return element
+    new = {}
+    for country, v in element.items():
+        # Production - Export
+        if math.isclose(v, 0.0):
+            domestic = 0.0
         else:
-            fraction = common_get_export_fraction(country)
-            cached_export_fraction[country] = fraction
-        return fraction
+            fraction = cached_get_export_fraction(country)
+            domestic = (1 - fraction) * v
+        # Import
+        total_import = 0
+        for cc, vv in element.items():
+            if cc == country:
+                # Skip self
+                continue
+            total_import += cached_get_import_fraction(cc, country) * vv
+        new[country] = domestic + total_import
+    # Now we do non-masterdata countries, which only do
+    # imports
+    for country in non_masterdata_alpha2:
+        total_import = sum(
+            cached_get_import_fraction(cc, country) * vv for cc, vv in element.items()
+        )
+        new[country] = total_import
+    return new
 
-    def get_import_fraction(e, i):
-        key = f"{e}_{i}"
-        if key in cached_import_fraction:
-            fraction = cached_import_fraction[key]
-        else:
-            fraction = common_get_import_fraction(e, i)
-            cached_import_fraction[key] = fraction
-        return fraction
 
+def modify_array_based_on_coal_export(arr, production_2019):
     for i in range(len(arr)):
-        element = arr[i]
-        if isinstance(element, float):
-            continue
-        new = {}
-        for country, v in element.items():
-            # Production - Export
-            if math.isclose(v, 0.0):
-                domestic = 0.0
-            else:
-                fraction = get_export_fraction(country)
-                domestic = (1 - fraction) * v
-            # Import
-            total_import = 0
-            for cc, vv in element.items():
-                if cc == country:
-                    # Skip self
-                    continue
-                total_import += get_import_fraction(cc, country) * vv
-            new[country] = domestic + total_import
-        # Now we do non-masterdata countries, which only do
-        # imports
-        for country in non_masterdata_alpha2:
-            total_import = sum(
-                get_import_fraction(cc, country) * vv for cc, vv in element.items()
-            )
-            new[country] = total_import
-
+        new = modify_based_on_coal_export(arr[i])
         arr[i] = new
