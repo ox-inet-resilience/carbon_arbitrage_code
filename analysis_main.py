6f39e695ec1365165addb3f80724299229617580
climatefinance <105055284+climatefinance@users.noreply.github.com>
2024-10-14 16:09:23 -0400

    Update unit profit data

--- analysis_main.py
+++ analysis_main.py
@@ -66,7 +66,7 @@ def pandas_divide_or_zero(num, dem):
 ngfs_df = util.read_ngfs()
 iso3166_df = util.read_iso3166()
 unit_profit_df = pd.read_csv(
-    "data_private/v3_bbg_main_country_region_av_profitability.csv.zip",
+    "data_private/v4_final_country_region_profit_analysis.csv.zip",
     compression="zip",
 )
 alpha2_to_alpha3 = iso3166_df.set_index("alpha-2")["alpha-3"].to_dict()
